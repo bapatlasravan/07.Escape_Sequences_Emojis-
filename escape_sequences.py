5457a8b8f3f8332951a8cf6edc63606ff6584a36
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-05-10 13:55:42 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,37 @@
+"""
+Day 07
+Date: 27thth April 2022 wednesday
+Name: Bapatla sravan
+File Desc: sscape_sequences & emojis
+"""
+# print('''Hi  \
+# My name is Bapatla Sravan, \
+# My home town is Bapatla''') #Hi  My name is Bapatla Sravan, My home town is Bapatla
+
+
+# print('programming\'language') #programming'language
+# print("programming'language") #programming'language
+
+
+# print('Hello'   'World') #HelloWorld
+# print('Hello\n''World')#Hello
+#                        #World
+# print('Hello'   '\nWorld')#Hello
+#                           #World
+# print('Hello'  , 'World',sep='\n') #Hello
+#                                    #World
+# print('Hello'+'\n'+'World') #Hello
+#                             #World
+
+# print(' C:/Users/bapat/AppData/Local/Programs/Python/Python310/python') # C:/Users/bapat/AppData/Local/Programs/Python/Python310/python
+# print(' C:\Users\bapat\AppData\Local\Programs\Python\Python310\python')# error
+# print(' C:\\Users\bapat\AppData\Local\Programs\Python\Python310\python') # C:\Userapat\AppData\Local\Programs\Python\Python310\python
+
+print('\U0001F601')
+print('\U0001F622')
+print('\U0001F61C')
+print('\U0001F92A')
+print('\U0001F61D')
+print('\U0001F911')
+print('\U0001F611')
+print('\U0001F636')
